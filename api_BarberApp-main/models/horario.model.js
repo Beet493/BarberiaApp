b492e39f3f4aaa99f59b84c6a612f0f6a71c9afd
Beet493 <julizandro11@gmail.com>
2026-05-28 12:45:17 -0600

    Ya esta la pagina 2.0

--- api_BarberApp-main/models/horario.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const HorarioSchema = mongoose.Schema({
-    dia: { type:String,required:true},
-    apertura:{type:String,required:true},
-    cierre:{type:String,required:true}
-},{timestamps:true})
-
-module.exports = mongoose.model("horario",HorarioSchema)
